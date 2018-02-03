2c4ba6fb8740799350f0904a3e23dc7028769bda
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-02-02 19:12:15 -0500

    starting MEAN

---
+++ JSFundamentals/sandbox.js
@@ -0,0 +1,5 @@
+console.log('\u263A \u2603 \u272f');
+let haiku = "Having been erased, \nThe document you're seeking \nMust now be retyped."
+console.log(haiku);
+
+var names = ['Kadie', 'Joe', 'Fritz', 'Pierre', 'Alphonso'];
