04a82c7054fe87fc9a00789a11dae030d36dfda6
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-02-08 16:44:31 -0500

    API

---
+++ Mongo/1955_API/server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./server/config/mongoose.js');
+
+var routes_setter = require('./server/config/routes.js');
+routes_setter(app);
+
+app.listen(8001, function(){
+    console.log('listening on port 8001');
+})
