ee9eb148ff660cc5fe6fef4d5dd0560f85777f9a
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-03-22 22:04:21 -0400

    _

--- projects/portfolio/server/config/routes.js
+++ projects/portfolio/server/config/routes.js
@@ -1,5 +1,6 @@
 const User = require("../controllers/userController");
 const Image = require("../controllers/imageController");
+const Project = require("../controllers/projectController");
 const path = require("path");
 
 module.exports = (app) => {
@@ -16,7 +17,10 @@ module.exports = (app) => {
     app.get("/images", Image.getImages);
     app.delete("/image/:id", Image.deleteImage);
 
-  
+    // 
+    app.post("/create/project", Project.create);
+    app.get("/projects", Project.show);
+    
 
     // talk to the front end
     app.all("*", (req, res, next) => {
